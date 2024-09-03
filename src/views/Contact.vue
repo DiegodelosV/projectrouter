7cbbf516a2f384eee3da0d2324207d990a7f1805
DiegodelosV <diegodelosv@gmail.com>
2024-09-03 17:02:49 -0400

    actualziacion botones

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,3 +1,27 @@
-<template lang="">
+<template>
   <h1>Información de contacto</h1>
+  <br />
+  <div>
+    <button class="button-index" @click="volverInicio">Volver al inicio</button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Contact",
+  methods: {
+    volverInicio() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style>
+.button-index {
+  margin-top: 10px;
+  padding: 5px;
+  color: white;
+  background-color: #42b983;
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,10 +1,35 @@
 <template>
-    <h1>Información de los Productos</h1>
-    <p>Estos son los Productos</p>
-    <ul>
-        <li>Producto 1</li>
-        <li>Producto 2</li>
-        <li>Producto 3</li>
-    </ul>
+  <h1>Información de los Productos</h1>
+  <p>Estos son los Productos</p>
+  <ul>
+    <li>Producto 1</li>
+    <li>Producto 2</li>
+    <li>Producto 3</li>
+  </ul>
+  <div>
     <router-link to="/">Volver</router-link>
+    <br />
+    <button class="button-index" @click="volverInicio">Volver al inicio</button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Products",
+  methods: {
+    volverInicio() {
+      return;
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style>
+.button-index {
+  margin-top: 10px;
+  padding: 5px;
+  color: white;
+  background-color: #42b983;
+}
+</style>
